5f82f3995fdb3e31742ad42b9294ff617d14d1d4
Eric Keis <ericpkeis@gmail.com>
2023-11-07 20:19:38 -0700

    Add progress bar to CLI

--- port_scanner/scanner.py
+++ port_scanner/scanner.py
@@ -2,6 +2,7 @@ import os
 import socket
 import concurrent.futures
 from datetime import datetime
+from tqdm import tqdm
 
 def create_output_directory():
     directory_name = 'port_scanner_results'
@@ -18,7 +19,7 @@ def scan_port(target_host, target_port):
     except (socket.timeout, socket.error):
         return False
 
-def port_scanner(target_host, port_range, output_filename):
+def port_scanner(target_host, port_range, output_filename, progress_bar=None):
     with open(output_filename, 'w') as output_file:
         output_file.write(f"Scanning target host \"{target_host}\" for ports {port_range}:\n")
 
@@ -33,6 +34,14 @@ def port_scanner(target_host, port_range, output_filename):
                         #output_file.write(f"Port {port} is closed\n")
                 except Exception as e:
                     output_file.write(f"An error occurred while scanning port {port}: {e}\n")
+                finally:
+                        # Update the progress bar
+                        if progress_bar:
+                            progress_bar.update(1)
+
+        if progress_bar:
+          progress_bar.update()
+          progress_bar.close()
 
 def run():
   try:
@@ -43,10 +52,12 @@ def run():
       if start_port > end_port or not (1 <= start_port <= 65535) or not (1 <= end_port <= 65535):
           raise ValueError("Invalid port range")
 
+      progress_bar = tqdm(total=end_port - start_port + 1, desc="Scanning Ports", unit="port")
+
       output_directory = create_output_directory()
       timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
       output_filename = f"{output_directory}/scan_result_{timestamp}.txt"
-      port_scanner(target_host, (start_port, end_port), output_filename)
+      port_scanner(target_host, (start_port, end_port), output_filename, progress_bar)
 
       print(f"Scan results saved in {output_filename}")
   except ValueError as e:
